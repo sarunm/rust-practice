573b74d789ad1182e98a15e86df57cb66d1b653c
Sarun M <sarun.maksuanpan@krungsrinimble.com>
2024-12-19 21:47:45 +0700

    declare type

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,15 @@ fn main() {
     let x = 5;
     let y = 10.0;
     let z = x + y as i32;
+
+    println!("z is {}", z);
+
+    let msg1 = String::from("Hello, world!");
+    let msg2  = "Hello, world!".to_string();
+    let msg3 = "Hello, world!";
+    let msg4 = format!("Point: {},{}",x,y);
+
+    println!("msg4 is {}", msg4);
 }
 
 
