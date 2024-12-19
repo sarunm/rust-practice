879a26b147ca8b599508c4a70247fbe6422a0e9d
Sarun M <sarun.maksuanpan@krungsrinimble.com>
2024-12-19 22:23:14 +0700

    seperate to function

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 fn main() {
-    println!("Hello, crabby!");
+    data_types();
+    poc_if();
+    poc_match();
+    poc_loops();
+}
 
-    let mut n = 10;
-    n = 99;
-    println!("n is {}", n);
-
+fn data_types() {
     let x = 5;
     let y = 10.0;
     let z = x + y as i32;
 
     println!("z is {}", z);
 
-    let msg1 = String::from("Hello, world!");
-    let msg2  = "Hello, world!".to_string();
-    let msg3 = "Hello, world!";
-    let msg4 = format!("Point: {},{}",x,y);
+    // let msg1 = String::from("Hello, world!");
+    // let msg2 = "Hello, world!".to_string();
+    // let msg3 = "Hello, world!";
+    let msg4 = format!("Point: {},{}", x, y);
 
     println!("msg4 is {}", msg4);
+}
 
-
-    let check = 50 ;
+fn poc_if() {
+    let check = 50;
 
     if check > 100 {
         println!("check is greater than 100");
@@ -28,15 +30,25 @@ fn main() {
     } else {
         println!("check is equal to 100");
     }
+}
 
-
+fn poc_match() {
     let test_string = "test1";
 
     match test_string {
         "test1" => println!("test1"),
         "test2" => println!("test2"),
-        _ => println!("default")
+        _ => println!("default"),
     }
 }
 
-
+fn poc_loops() {
+    let mut count = 0;
+    let result = loop {
+        count += 1;
+        if count == 10 {
+            break count;
+        }
+    };
+    println!("result is {}", result);
+}
